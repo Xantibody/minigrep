38c8de08c189536e696415835793eff738654df3
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-21 00:08:41 +0900

    refactor(rs): Config structを実装

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ fn main() {
     // {}というファイルの中
     println!("In file {}", filename);
 
-    let (query, filename) = parse_config(&args);
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
+}
+
+#[derive(Debug)]
+struct Config {
+    query: String,
+    filename: String,
 }
 
 fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+    let query = &args[1].clone();
+    let filename = &args[2].clone();
 
-    (query, filename)
+    Config(query, filename)
 }
