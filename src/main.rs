d022c71a3845cbb5a882456551382532598acd46
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-21 00:02:28 +0900

    refacotr: parse_configへ集約

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
     // {}を探しています
     println!("Searching for {}", query);
     // {}というファイルの中
     println!("In file {}", filename);
+
+    let (query, filename) = parse_config(&args);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
