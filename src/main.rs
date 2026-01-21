5b3cccb19e698c44e52f07ec64b4ed0fea5ac629
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-22 01:06:31 +0900

    feat: fileが開けるように

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
-
     let config = parse_config(&args);
 
     println!("Searching for {}", config.query);
@@ -25,4 +24,13 @@ fn parse_config(args: &[String]) -> (&str, &str) {
     let filename = &args[2].clone();
 
     Config(query, filename)
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        // ファイルの読込中に問題がありました
+        .expect("something went wrong reading the file");
+
+    // テキストは\n{}です
+    println!("With text:\n{}", contents);
 }
