8b10b6a669d72e2edde0069f2f40bb4f84bc7782
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-23 01:34:21 +0900

    fix: runの戻り値にErrを実装

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
@@ -14,15 +15,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("file not found");
+    run(config);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        // ファイルの読込中に問題がありました
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     // テキストは\n{}です
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 #[derive(Debug)]
