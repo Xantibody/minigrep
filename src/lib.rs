49f77f7408d5d622f4f1f39f72547bb481c493a7
Xantibody <zeku.bushinryu38@gmail.com>
2026-01-27 19:21:10 +0900

    feat: search_case_insensitive test

--- src/lib.rs
+++ src/lib.rs
@@ -28,10 +28,26 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     vec![]
 }
 
@@ -40,15 +56,31 @@ mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         // RUstは
         // 安全で速く生産性も高い。
         // 3つ選んで。
         let contents = "\
-                        Rust:
-                        safe, fast, productive.
-                        Pick three,";
+Rust:
+safe, fast, productive.
+Pick three,";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        // (最後の行のみ)
+        // 私を信じて
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
